ac5b1ca9a6581c633b10312014a33e148dafe6f0
juiceman1014 <justintran1014@gmail.com>
2024-12-01 22:57:54 -0800

    script can now automate testing for one image

--- gauth_test.py
+++ gauth_test.py
@@ -1,5 +1,9 @@
 from appium import webdriver
 from appium.options.common import AppiumOptions
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 options = AppiumOptions()
 options.set_capability("platformName", "Android")
@@ -15,6 +19,73 @@ driver = webdriver.Remote(
 )
 
 print("Session started successfully!")
-print(driver.page_source)
+agree_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/agreeBtn")))
+agree_button.click()
+
+continue_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/btn")))
+continue_button.click()
+
+continue_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/btn")))
+continue_button.click()
+
+continue_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/btn")))
+continue_button.click()
+
+continue_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/btn")))
+continue_button.click()
+
+time.sleep(3)
+
+exit_button = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("com.lynx.tasm.behavior.ui.LynxFlattenUI").instance(1)')
+exit_button.click()
+
+permission_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.android.permissioncontroller:id/permission_allow_foreground_only_button")))
+permission_button.click()
+
+permission_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.android.permissioncontroller:id/permission_deny_button")))
+permission_button.click()
+
+time.sleep(3)
+
+math_button = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Math")')
+math_button.click()
+
+input_image_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/pickAlbum")))
+input_image_button.click()
+
+permission_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.android.permissioncontroller:id/permission_allow_all_button")))
+permission_button.click()
+
+time.sleep(1)
+
+select_image_button = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, "new UiSelector().resourceId(\"com.education.android.h.intelligence:id/photoPreview\").instance(0)")
+select_image_button.click()
+
+submit_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/btnSubmit")))
+submit_button.click()
+
+time.sleep(10)
+
+answer_container = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((AppiumBy.ID, "com.education.android.h.intelligence:id/answerContentContainer"))
+)
+
+child_elements = answer_container.find_elements(AppiumBy.XPATH, ".//*")
+
+container_text = "".join([child.text for child in child_elements if child.text.strip()])
+container_text = container_text.replace(" ", "")
+print("Answer Container Text: ", container_text)
+
+expected_output = "4𝑎+6"
+
+if expected_output in container_text:
+    print("Test passed")
+else:
+    print("Test failed")
+
+return_home_button = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, "new UiSelector().className(\"android.widget.ImageView\").instance(0)")
+return_home_button.click()
+
+time.sleep(5)
 
 driver.quit()
