f6807ace152b5bd10b657fafb8769cbc40a00ca8
juiceman1014 <justintran1014@gmail.com>
2024-12-02 20:08:35 -0800

    script scrolls to next page after all images are processed

--- gauth_test.py
+++ gauth_test.py
@@ -3,6 +3,9 @@ from appium.options.common import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.pointer_input import PointerInput
 import time
 
 #configurations for Gauth and emulator
@@ -21,30 +24,47 @@ driver = webdriver.Remote(
 )
 print("\nSession started!\n")
 
+#when all images on current page are processes, scroll to next page.
+def swipe_to_next_page():
+    touch_action = PointerInput("touch", "touch")
+    actions = ActionBuilder(driver, mouse=touch_action)
+    
+    actions.pointer_action.move_to_location(561, 1782)
+    actions.pointer_action.pointer_down()
+    actions.pointer_action.move_to_location(561, 438)
+    actions.pointer_action.pointer_up()
+    actions.perform()
+
+    actions = ActionBuilder(driver, mouse=touch_action)
+    actions.pointer_action.move_to_location(594, 1618)
+    actions.pointer_action.pointer_down()
+    actions.pointer_action.move_to_location(594, 1040)
+    actions.pointer_action.pointer_up()
+    actions.perform()
+
+    actions = ActionBuilder(driver, mouse=touch_action)
+    actions.pointer_action.move_to_location(569, 455)
+    actions.pointer_action.pointer_down()
+    actions.pointer_action.move_to_location(569, 365)
+    actions.pointer_action.pointer_up()
+    actions.perform()
+
 passed_tests = 0
 failed_tests = 0
 expected_outputs = [
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
     "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
     "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
-    "30", "31", "32", "33", "34", "35", "36", "37", "38", "39"
+    "30", "31", "32", "33", "34"
 ]
 
 #onboarding stage
 agree_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/agreeBtn")))
 agree_button.click()
 
-continue_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/btn")))
-continue_button.click()
-
-continue_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/btn")))
-continue_button.click()
-
-continue_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/btn")))
-continue_button.click()
-
-continue_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/btn")))
-continue_button.click()
+for _ in range(4):
+    continue_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/btn")))
+    continue_button.click()
 
 time.sleep(3)
 
@@ -111,6 +131,7 @@ try:
         print("----------------------------\n")
         failed_tests+=1
 except Exception as e:
+    expected_output = expected_outputs[0]
     error_dismiss_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/container")))
     error_dismiss_button.click()
     print("----------------------------")
@@ -124,17 +145,26 @@ finally:
     return_home_button.click()
 
 #loops through remaining images in album
-for i in range(1,40):
+for i in range(1,35):
     try:
         #click on image album
         input_image_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/pickAlbum")))
         input_image_button.click()
 
+        if(i >= 18):
+            print("Swiping")
+            swipe_to_next_page()
+
         time.sleep(1)
 
-        #pick the image
-        select_image_button = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, f'new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().resourceId("com.education.android.h.intelligence:id/photoPreview").instance({i}))')
-        select_image_button.click()
+        try:
+            #pick the image
+            select_image_button = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, f'new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().resourceId("com.education.android.h.intelligence:id/photoPreview").instance({i % 18}))')
+            select_image_button.click()
+        except Exception as e:
+            print(f'Image instance {i} not found after scrolling: {e}')
+            failed_tests+=1
+            continue
 
         #submit the image
         submit_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/btnSubmit")))
@@ -169,6 +199,7 @@ for i in range(1,40):
             print("----------------------------\n")
             failed_tests+=1
     except Exception as e:
+        expected_output = expected_outputs[i]
         error_dismiss_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((AppiumBy.ID, "com.education.android.h.intelligence:id/container")))
         error_dismiss_button.click()
         print("----------------------------")
@@ -186,7 +217,7 @@ time.sleep(5)
 
 print(f'Passed tests: {passed_tests}')
 print(f'Failed tests: {failed_tests}')
-print(f'Pass rate: {passed_tests}/2')
+print(f'Pass rate: {passed_tests}/35')
 print(f'Pass percentage: {(passed_tests/2) * 100}%')
 
 driver.quit()
